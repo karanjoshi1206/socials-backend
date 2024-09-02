624e3e4fbbec769c9903ba0165067497488b6685
KARAN JOSHI <64606159+karanjoshi1206@users.noreply.github.com>
2024-09-02 21:21:22 +0530

    ADDED env PORT

--- app.ts
+++ app.ts
@@ -8,7 +8,7 @@ import socialRouter from "./routes/socials.js";
 dotenv.config();
 const app = express();
 app.use(cors());
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use("/socials", socialRouter);
 
